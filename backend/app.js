b932332176f6c2d69077f992c55b1c653fdec03b
mytriagoco@gmail.com <mytriagoco@gmail.com>
2022-03-19 12:26:57 -0400

    .

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,8 @@ var mongoMask = require('mongo-mask')
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-const app = express();goose.connect('mongodb+srv://project6:Project6OpenClassroom@cluster0.c189u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-.then(() => {
-mon
+const app = express();
+mongoose.connect('mongodb+srv://project6:Project6OpenClassroom@cluster0.c189u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => {
 console.log('Successfully connected to MongoDb Atlas');
 }).catch((error) => {
 console.log('Unable to connect to MongoDb Atlas');
